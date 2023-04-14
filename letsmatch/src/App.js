c633bda2fff1366eb53baaa12c9c678a0022e41c
carolinaloureiro24 <carolina.loureiro@ccg.pt>
2023-04-14 19:36:18 +0100

    Hello 2

--- letsmatch/src/App.js
+++ letsmatch/src/App.js
@@ -1,25 +1,124 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { useToggle, upperFirst } from '@mantine/hooks';
+import {
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  PaperProps,
+  Button,
+  Divider,
+  Checkbox,
+  Anchor,
+  Stack,
+} from '@mantine/core';
 
+// import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
+
+
+  
+
+function AuthenticationForm(props: PaperProps) {
+  const [type, toggle] = useToggle(['login', 'register']);
+  const form = useForm({
+    initialValues: {
+      email: '',
+      name: '',
+      password: '',
+      terms: true,
+    },
+
+    validate: {
+      email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
+      password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
+    },
+  });
+
+  return (
+    <Paper radius="md" p="xl" withBorder {...props}>
+      <Text size="lg" weight={500}>
+        Welcome to LET'SMATCH!, {type} with
+      </Text>
+
+
+      <Divider label="Or continue with email" labelPosition="center" my="lg" />
+
+      <form onSubmit={form.onSubmit(() => {})}>
+        <Stack>
+          {type === 'register' && (
+            <TextInput
+              label="Name"
+              placeholder="Your name"
+              value={form.values.name}
+              onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+              radius="md"
+            />
+          )}
+
+          <TextInput
+            required
+            label="Email"
+            placeholder="hello@email.pt"
+            value={form.values.email}
+            onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+            error={form.errors.email && 'Invalid email'}
+            radius="md"
+          />
+
+          <PasswordInput
+            required
+            label="Password"
+            placeholder="Your password"
+            value={form.values.password}
+            onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+            error={form.errors.password && 'Password should include at least 6 characters'}
+            radius="md"
+          />
+
+          {type === 'register' && (
+            <Checkbox
+              label="I accept terms and conditions"
+              checked={form.values.terms}
+              onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+            />
+          )}
+        </Stack>
+
+        <Group position="apart" mt="xl">
+          <Anchor
+            component="button"
+            type="button"
+            color="dimmed"
+            onClick={() => toggle()}
+            size="xs"
+          >
+            {type === 'register'
+              ? 'Already have an account? Login'
+              : "Don't have an account? Register"}
+          </Anchor>
+          <Button type="submit" radius="xl">
+            {upperFirst(type)}
+          </Button>
+        </Group>
+      </form>
+    </Paper>
+  )
+          }
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AuthenticationForm />
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
